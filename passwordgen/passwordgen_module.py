d7104143c7e0a49ef5df0dbb815cf2f1b9bbb514
Balazs Ando <elnesia@gmail.com>
2016-05-30 14:50:09 +0200

    passwordgen solved

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,28 @@
-def passwordgen():
-    return
+import random
+import string
+
+
+def passwordgen(diff=""):
+    result = ""
+    if diff == "weak":
+        weakpw = ["poopyhead123", "sleepyface1990", "ihavenoidea"]
+        result = random.choice(weakpw)
+    else:
+        chars = string.ascii_letters + string.digits + "*-+_!?"
+        strongpw = "".join(random.choice(chars) for _ in range(random.randrange(8, 21)))
+        result = strongpw
+    return result
 
 
 def main():
+    while True:
+        request = input("Request new password? (Y/N)")
+        if request == "Y":
+            global diff
+            diff = input("Strenght of password? (weak/strong)")
+            print(passwordgen(diff))
+        else:
+            break
     return
 
 
