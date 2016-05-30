0f4066b53bc14f69181e0f681550316c15770efb
Balazs Ando <elnesia@gmail.com>
2016-05-30 13:42:04 +0200

    listoverlap solved

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,8 +1,16 @@
+import random
+
+
 def listoverlap(list1, list2):
-    return
+    return list(set(list1) & set(list2))
 
 
 def main():
+    # a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    # b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    a_rand = random.sample(range(1, 101), random.randrange(1, 21))
+    b_rand = random.sample(range(1, 101), random.randrange(1, 21))
+    listoverlap(a_rand, b_rand)
     return
 
 
