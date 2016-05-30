055d746be560f63d7c09f4e510d387d3d48cb4f1
Balazs Ando <elnesia@gmail.com>
2016-05-30 11:55:23 +0200

    years solved

--- years/years_module.py
+++ years/years_module.py
@@ -2,12 +2,19 @@ import datetime
 
 
 def years(age):
-    return
+    remaining_years = 100 - age
+    current_year = datetime.datetime.now()
+    hundred_years = current_year.year + remaining_years
+    return hundred_years
 
 
 def main():
+    name = input("What is your name?")
+    age = int(input("What is your age?"))
+    repeat = int(input("How many times to repeat the message?"))
+    print(("You will be 100 years old in %d.\n" % years(age)) * repeat)
     return
 
-
+    
 if __name__ == '__main__':
     main()
