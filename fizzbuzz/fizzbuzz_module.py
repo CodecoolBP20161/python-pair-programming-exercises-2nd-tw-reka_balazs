26b254b8ef6584c18ad01237fd1eae7e578eff96
rekan <rekanagy06@gmail.com>
2016-06-03 09:36:34 +0200

    solved with 2 if. cause we can!

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,20 +1,15 @@
 def fizzbuzz(number):
     to_return = ""
-    if number % 15 == 0:
-        to_return = "FizzBuzz"
-    elif number % 5 == 0:
-        to_return = "Buzz"
-    elif number % 3 == 0:
+    if number % 3 == 0:
         to_return = "Fizz"
-    else:
-        to_return = number
-    return to_return
+    if number % 5 == 0:
+        to_return += "Buzz"
+    return to_return or number
 
 
 def main():
     for i in range(1, 101):
-        num = fizzbuzz(i)
-        print(num)
+        print(fizzbuzz(i))
     return
 
 if __name__ == '__main__':
